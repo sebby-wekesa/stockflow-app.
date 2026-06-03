3d393f0fc6b797f121ff80674b1b41a89222b8eb
sebby-wekesa <sebbywakis@gmail.com>
2026-06-03 11:57:47 +0300

    improve the /operator page

--- stockflow/prisma/migrations/20260603001000_product_uom_kg/migration.sql
+++ stockflow/prisma/migrations/20260603001000_product_uom_kg/migration.sql
@@ -1,14 +1,14 @@
-UPDATE "Product"
-SET "uom" = 'KG';
-
-ALTER TABLE "Product"
-ALTER COLUMN "uom" SET DEFAULT 'KG';
-
 ALTER TABLE "Product"
 DROP CONSTRAINT IF EXISTS "Product_uom_pcs_sets_check";
 
 ALTER TABLE "Product"
 DROP CONSTRAINT IF EXISTS "Product_uom_kg_check";
+
+UPDATE "Product"
+SET "uom" = 'KG';
+
+ALTER TABLE "Product"
+ALTER COLUMN "uom" SET DEFAULT 'KG';
 
 ALTER TABLE "Product"
 ADD CONSTRAINT "Product_uom_kg_check"
